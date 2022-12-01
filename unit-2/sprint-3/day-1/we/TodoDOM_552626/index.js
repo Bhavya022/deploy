9a4c4b558eb0254bf6787c4127846ee04eec0c8d
Bhavya022 <dnd88778@gmail.com>
2022-12-01 20:04:31 +0530

    puppy.html added

--- unit-2/sprint-3/day-1/we/TodoDOM_552626/index.js
+++ unit-2/sprint-3/day-1/we/TodoDOM_552626/index.js
@@ -1,6 +1,28 @@
 let myform = document.getElementById("form");
-let body = document.querySelector("tbody");
-let data = [];
+let body = document.querySelector("tbody"); 
+
+let a=10;
+
+let data =JSON.parse( localStorage.getItem("UserData"));
+if(data==null){
+    data=[];
+} 
+data.forEach((element,index)=>{
+
+    let tr = document.createElement("tr"); 
+    let td1 = document.createElement("td");
+    let td2 = document.createElement("td");   
+    let td3 = document.createElement("td");
+    td1.innerText = element.task;
+    td2.innerText = element.priority; 
+    td3.innerText="Favourite";
+    if(element.priority=="High"){
+    td2.style.backgroundColor="red";
+    } 
+    else{
+        td2.style.backgroundColor="green";
+    } 
+});
 myform.addEventListener("submit",(event)=>{
 event.preventDefault();
 
@@ -10,17 +32,30 @@ let formDara ={
 } ;
 //console.log(formDara); 
 data.push(formDara); 
+localStorage.setItem("UserData",JSON.stringify(data));
 //console.log(data); 
 tbody.innerHTML = null;
 data.forEach((element,index)=>{
 
     let tr = document.createElement("tr"); 
     let td1 = document.createElement("td");
-    let td2 = document.createElement("td");  
+    let td2 = document.createElement("td");   
+    let td3 = document.createElement("td");
     td1.innerText = element.task;
-    td2.innerText = element.priority;
-    tr.append(td1,td2);
+    td2.innerText = element.priority;  
+    td3.innerText="Favourite"; 
+    td3.addEventListener("click",()=>{
+        console.log(element);
+    })
+    if(element.priority=="High"){
+    td2.style.backgroundColor="red";
+    } 
+    else{
+        td2.style.backgroundColor="green";
+    }
+    tr.append(td1,td2,td3);
     tbody.append(tr);
 });
 
 }) ;
+ 
