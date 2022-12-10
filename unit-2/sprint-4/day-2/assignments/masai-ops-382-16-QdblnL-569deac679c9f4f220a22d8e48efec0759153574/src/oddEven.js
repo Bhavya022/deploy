5e472f07acefa8900e6a50d1c8428e280633a9bd
Bhavya022 <dnd88778@gmail.com>
2022-12-10 20:10:06 +0530

    promise assignment updated

--- unit-2/sprint-4/day-2/assignments/masai-ops-382-16-QdblnL-569deac679c9f4f220a22d8e48efec0759153574/src/oddEven.js
+++ unit-2/sprint-4/day-2/assignments/masai-ops-382-16-QdblnL-569deac679c9f4f220a22d8e48efec0759153574/src/oddEven.js
@@ -1,5 +1,25 @@
 function getData(data) {
-    
+    let promise = new Promise((resolve,reject)=>{ 
+       if(isNaN(data)==false){
+        reject("error");
+       } else if(data%2==0){ 
+        setTimeout(()=>{
+       resolve("even");
+        },2000);
+       } 
+       else{ 
+        setTimeout(()=>{
+      resolve("odd");
+        },4000)
+       }
+    })
 }
-
-export default getData
+let abcd = getData(data); 
+    console.log(abcd); 
+    abcd.then((res)=>{
+        console.log(abcd);
+    }) 
+    .catch((error)=>{
+        console.log(abcd);
+    });
+export default getData ; 
