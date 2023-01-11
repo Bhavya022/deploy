577a505bc4ea489d831304af6e0cebfc862816be
Bhavya022 <dnd88778@gmail.com>
2023-01-11 21:59:09 +0530

    ls done

--- unit-3/sprint-4/day-1/you/masai-ops-638-1329-hCGIrz-30810f7b6d93e85187bba8286926af6f4e940b86/Scripts/index.js
+++ unit-3/sprint-4/day-1/you/masai-ops-638-1329-hCGIrz-30810f7b6d93e85187bba8286926af6f4e940b86/Scripts/index.js
@@ -4,29 +4,33 @@ var desc = document.getElementById("desc").value;
 var date = document.getElementById("addDate").value;
 var dead = document.getElementById("deadline").value
 var pri = document.getElementById("priority").value ;
-var submit = document.getElementById("submit").value; 
-console.log(name,desc,date,dead,pri)
+var submit = document.getElementById("submit");  
+let cont = document.getElementById("cont") 
+let user =[] ||JSON.parse(localStorage.getItem("todos"))
+//console.log(name,desc,date,dead,pri)
 function store(){
     var name = document.getElementById("name").value;
 var desc = document.getElementById("desc").value;
 var date = document.querySelector('input[type="date"]').value;
 var dead = document.getElementById("deadline").value
 var pri = document.getElementById("priority").value ;
-
+var status = "todo"
 const obj={
     name:name,
     desc:desc,
     date:date,
     dead:dead,
-    pri:pri,
+    pri:pri, 
+    status
 }   
 //console.log(obj) 
-//alert(obj)
-  localStorage.setItem("todos",JSON.stringify(obj)) 
-  localStorage.setItem("status",JSON.stringify("todo"))
+//alert(obj) 
+user.push(obj)
+  localStorage.setItem("todos",JSON.stringify(user)) 
+ // localStorage.setItem("status",)
 
-}
+} 
 
-
+cont.addEventListener("submit",store)
 
 
