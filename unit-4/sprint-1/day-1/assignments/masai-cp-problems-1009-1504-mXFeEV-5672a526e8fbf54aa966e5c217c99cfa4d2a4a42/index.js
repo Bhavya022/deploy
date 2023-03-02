df8767a76df6555be837ccd7468b28cc996184ec
Bhavya022 <dnd88778@gmail.com>
2023-03-02 14:50:37 -0800

    doneeeeeeeeeeeee

--- unit-4/sprint-1/day-1/assignments/masai-cp-problems-1009-1504-mXFeEV-5672a526e8fbf54aa966e5c217c99cfa4d2a4a42/index.js
+++ unit-4/sprint-1/day-1/assignments/masai-cp-problems-1009-1504-mXFeEV-5672a526e8fbf54aa966e5c217c99cfa4d2a4a42/index.js
@@ -4,7 +4,8 @@
 
 
 
-//  get a commands using process.argv
+//  get a commands using process.argv 
+const crypto = require('crypto')
 const argvs = process.argv
 const argv = argvs.slice(2)
 const operation = argv[0]
@@ -39,7 +40,26 @@ case 'cos':
   console.log(Math.cos(operator1)) 
   break 
   case 'tan':
-    console.log(Math.tan(operator1))
+    console.log(Math.tan(operator1)) 
+    break 
+    case 'random':
+      crypto.randomBytes(operator1, (err, buf) => {
+        if (err) {
+          // Prints error
+          console.log("Provide length for random number generation.");
+          return;
+        }
+        else{
+          
+        if(buf.length==0){
+          console.log("Provide length for random number generation."); 
+          return 
+        }
+        // Prints random bytes of generated data
+        console.log(buf.toString('binary'));
+        }
+      });
+      break
   default:
     console.log("Invalid operation");
 }
