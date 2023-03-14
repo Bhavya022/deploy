552e0b09902098a51b8bccfa7c55b573977c4e75
Bhavya022 <dnd88778@gmail.com>
2023-03-15 01:30:30 +0530

    file upload done

---
+++ unit-4/sprint-3/day-1/assignments/masai-cp-problems-1130-1612-YNxDOl-78426c5b70c0245f50f95514f3f5beb41c09f701/src/index.js
@@ -0,0 +1,37 @@
+// import required modules
+const express = require("express") 
+const multer = require("multer") 
+const app=express() 
+
+const storage = multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,'./uploads')
+    },
+    filename:function(req,file,cb){
+       //const uniqueSuffix = Date.now()+'-'+Math.round() 
+     cb(null,file.originalname)
+    }
+})  
+
+const upload = multer({storage:storage})
+app.post("/upload",upload.single('avatar'),(req,res)=>{
+    res.status(200).send({
+        message: "file uploaded successfully"
+     }) 
+    console.log(req.file) 
+})  
+app.get("/",(req,res)=>{
+    res.status(200).send({
+        message: "welcome to server"
+     })
+})
+function doit(x,y){
+    return x?x+y:y
+}
+app.listen(8000,()=>{
+    console.log("hello")
+}) 
+
+
+// export the server
+module.exports = app;
