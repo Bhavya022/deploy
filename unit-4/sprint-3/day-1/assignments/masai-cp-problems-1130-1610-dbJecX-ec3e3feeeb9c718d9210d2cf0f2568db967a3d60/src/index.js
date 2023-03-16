4430a05bd5b943b107afcc881405df5c4a69548e
Bhavya022 <dnd88778@gmail.com>
2023-03-16 17:46:07 +0530

    ddddd

--- unit-4/sprint-3/day-1/assignments/masai-cp-problems-1130-1610-dbJecX-ec3e3feeeb9c718d9210d2cf0f2568db967a3d60/src/index.js
+++ unit-4/sprint-3/day-1/assignments/masai-cp-problems-1130-1610-dbJecX-ec3e3feeeb9c718d9210d2cf0f2568db967a3d60/src/index.js
@@ -1,10 +1,11 @@
 //  import required modules from nodejs and build the server 
 const express = require("express") 
 const fs = require("fs") 
-const logger = require("../src/middlewares")
-const app = express() 
-
-get.post("/",(req,res)=>{
+const validator = require("../src/middlewares/validator")
+const app = express()  
+app.use(express.json())
+app.use(validator)
+app.post("/",(req,res)=>{
 
 })
 // export the server
--- unit-4/sprint-3/day-1/assignments/masai-cp-problems-1130-1610-dbJecX-ec3e3feeeb9c718d9210d2cf0f2568db967a3d60/src/middlewares/validator.js
+++ unit-4/sprint-3/day-1/assignments/masai-cp-problems-1130-1610-dbJecX-ec3e3feeeb9c718d9210d2cf0f2568db967a3d60/src/middlewares/validator.js
@@ -1,6 +1,33 @@
-const fs = require("fs");
+const express = require("express")
+const fs = require("fs"); 
 
+const validator = express.Router()
 // make the validator function and export it.
+validator.use((req,res,next)=>{
+    const {ID,Name,Rating,Description,Genre,Cast} = req.body 
+    if(!ID||!Name ||!Rating||!Description||!Genre||!Cast){
+        res.status(400).send("invalid request body.")
+    }
+    
+    if(typeof ID=="number" && typeof Name=="string" && typeof Rating=="number" && typeof Description=="string" && typeof Genre=="string" && Array.isArray(Cast) && Cast.every(ele=>
+        typeof ele=="string") && !Name.match(/[0-9]/g)) { 
+            fs.appendFileSync("./res.txt","ID is a number\n")  
+            fs.appendFileSync("./res.txt","Name is a string\n")  
+            fs.appendFileSync("./res.txt","Description is a string\n") 
+            fs.appendFileSync("./res.txt","Rating is a number\n") 
+            fs.appendFileSync("./res.txt","Genre is a string\n") 
+            fs.appendFileSync("./res.txt","Cast is a array of string\n")
+            
 
+            res.status(200).send("data received")
+        } 
+        else{
+            fs.appendFileSync("./res.txt","bad request.some data is incorrect.\n")
+            req.status(400).send("bad request.some data is incorrect.") 
+           
+        } 
+        next() 
+}) 
 
 // module.exports = validatorfunction;
+module.exports=validator
