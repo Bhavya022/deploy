bc89a019195a672f1df0abff84d5acff72937bcf
Bhavya022 <dnd88778@gmail.com>
2023-03-16 14:22:41 +0530

    Doneee

---
+++ unit-4/sprint-3/day-1/assignments/masai-cp-problems-1130-1610-dbJecX-ec3e3feeeb9c718d9210d2cf0f2568db967a3d60/src/index.js
@@ -0,0 +1,11 @@
+//  import required modules from nodejs and build the server 
+const express = require("express") 
+const fs = require("fs") 
+const logger = require("../src/middlewares")
+const app = express() 
+
+get.post("/",(req,res)=>{
+
+})
+// export the server
+module.exports=app 
