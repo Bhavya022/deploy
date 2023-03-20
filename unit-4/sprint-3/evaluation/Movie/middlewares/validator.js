c560e45451a73bedfa966e3f17008924bff8594f
Bhavya022 <dnd88778@gmail.com>
2023-03-20 16:56:48 +0530

    middleware done

---
+++ unit-4/sprint-3/evaluation/Movie/middlewares/validator.js
@@ -0,0 +1,42 @@
+const {check,validationResult}= require("express-validator") 
+require("dotenv").config()
+const bodyparser=require("body-parser") 
+const express = require("express") 
+const path=require("path") 
+const app=express() 
+const fs=require("fs")
+var PORT =process.env.port 
+
+const fieldanalyzer=(req,res,next)=>{
+    const {movie_name,
+        genre,
+        director,
+        rating,
+        year_of_release}=req.body  
+        if(!movie_name && !genre && !director && !rating && !year_of_release){
+            return res.send({
+                "err": "Few fields are missing, cannot process the request"
+                })
+        } 
+        else{
+            next() 
+        }
+} 
+
+const record = (req,res,next)=>{
+    const data = new Date() 
+    if(req.method=="PATCH"){
+        const msg=`The movie with id:${req.params.id} has been updated | ${date}./n` 
+        fs.appendFileSync("./record.txt",msg) 
+        res.send("record updated")
+    } 
+    else if(req.method=="DELETE"){
+        const msg=`The movie with id:${req.params.id} has been deleted | ${date}./n` 
+        fs.appendFileSync("./record.txt",msg) 
+        res.send("record deleted")
+    }  
+    else{
+        next() 
+    }
+} 
+module.exports={fieldanalyzer,record}
--- unit-4/sprint-3/evaluation/Movie/routes/movie.route.js
+++ unit-4/sprint-3/evaluation/Movie/routes/movie.route.js
@@ -15,20 +15,53 @@ movieRoute.get("/",async(req,res)=>{
      let data = await moviemodel.find().sort({[query]:val}).skip(skip).limit(limit)
     res.send(data)
 }) 
-movieRoute.patch("/",movie_patch) 
+movieRoute.patch("/:updateID",async(req,res)=>{
+    try{ 
+        console.log(req.body)
+const update_movie=await moviemodel.findByIdAndUpdate(req.params.updateID,req.body,{new:true});
+console.log(update_movie) 
+if(!update_movie){
+    res.status(400).send({msg:"movie not Found"})
+} 
+else{
+    res.send({msg:"movie updated successfully"})
+}
+    } 
+    catch(err){
+  res.status(500).send({err:err.message})
+    }
+    }) 
 movieRoute.post("/",async(req,res)=>{
     console.log(req.body) 
     try{
         const new_movie=new moviemodel(req.body) 
         await new_movie.save() 
-        res.status(200).send({msg:"movie addded successfully"}) 
+        res.status(200).send({msg:"movie addded successfully"})  
+        
           } 
           catch(err){ 
               
               console.log(err) 
-              res.send({err:err.message})
+              res.send({
+                "err": "Few fields are missing, cannot process the request"
+                }
+                )
           }
 }) 
-movieRoute.delete("/",movie_delete)  
+movieRoute.delete("/:deleteID",async(req,res)=>{
+    try{
+       const deleted_movie= await moviemodel.findByIdAndDelete(req.params.deleteID) 
+        console.log(deleted_movie)  
+        if(!deleted_movie){
+            res.status(404).send({msg:"movie not found"})
+        } 
+        else{
+            res.status(200).send({msg:"movie deleted successfully"})
+        }
+    } 
+    catch(err){
+     res.status(500).send({err:err.message})
+    }
+    })  
 
 module.exports={movieRoute}
