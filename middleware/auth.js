2d751b8f25fe2aebd60d8f497241f12960570247
Bhavya Khatri <115460646+Bhavya022@users.noreply.github.com>
2023-03-27 07:14:52 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ const jwt=require("jsonwebtoken")
 
 const auth=(req,res,next)=>{
 //logic 
-const token=req.headers.authorization.split("")[1] 
+const token=req.headers.authorization
 if(token){
     const decoded=jwt.verify(token,"masai")
     if(decoded){ 
